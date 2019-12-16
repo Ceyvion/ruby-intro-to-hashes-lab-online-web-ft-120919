a9b638ee23f47c398f7137bc8772f6249b7a8687
Ceyvion Biggs <ceyvionb@gmail.com>
2019-12-16 02:07:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,23 +9,19 @@ def actor
 end
 
 def monopoly
-	monopoly = {}
+	monopoly = {
+	  railroads: {}
+	}
 
 end
 
 def monopoly_with_second_tier
   
-  monopoly = {
-    railroads: {}
-  }
 
 end
 
 def monopoly_with_third_tier
-	monopoly = {
-    :railroads => {}
-  }
-
+	
 end
 
 def monopoly_with_fourth_tier
